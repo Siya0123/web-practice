c7aa2e785d7efd738a0c8e8192419a468ed32287
Siya0123 <96296552+Siya0123@users.noreply.github.com>
2021-12-17 04:53:48 -0800

    Add files via upload

---
+++ kabira.js
@@ -0,0 +1,40 @@
+console.log("I m tired btw what a co incidence dono k partners gayab hai");
+
+
+const kabir = {Name: "Kabir", Age: 16, Standard: "11th", Hobby: "Coding"}
+console.log(kabir.Name)
+document.getElementById("mcle").innerHTML = "Microsoft learn is a free site which helps to learn web developing a-z in a easy way.\
+\nYou can access 80 hours of content for free. \n With over 800 resources on web development it covers, Javascript, HTML, CSS and many more.";
+document.getElementById("aislinn").innerHTML = "Welcome to Shiva web practice set";
+document.getElementById("loop").innerHTML = "We are introducing a easy pratice set for html and js learner";
+document.getElementById("aislinn").style.textAlign = "center";
+document.getElementById("aislinn").style.color = "yellow";
+document.getElementById("loop").style.textAlign = "center";
+document.getElementById("loop").style.color = "yellow";
+document.getElementById("cwh").innerHTML = "Code with harry is a youtube channel. It will help you to learn almost every language with free video tutorials. \
+Full course of javascript with notes available on this channel also it provide full course of html and css. You can learn python, sql, java and many more\
+languages from this channel. Go and check it out.";
+document.getElementById("wsch").innerHTML = "W3 school is a free website to learn almost all programming language for free. Especially you learn Javascript, \
+HTML, CSS from basic to top. Also it includes video tutorial for HTML and also all references and DOM for free so visit on W3 schools.";
+document.getElementById("mdn").innerHTML = "MDN is one of the best source for learning Web Development.\
+ It's give in depth explanation about all the concept needed while learning Web development i.e. JavaScript, Html, CSS, React.\
+ It is a trustable website to learn web-development. MDN is my favourite website for Learning Web development."
+// document.getElementById("wihtml").innerHTML = "HTML (Hypertext Markup Language) is the code that is used to structure a web page and its content. For example, content could be structured within a set of paragraphs, a list of bulleted points, or using images and data tables."
+
+
+
+
+
+
+document.getElementsByClassName("list1").style.backgroundColor = "wheat";
+document.getElementById("intro").style.color = "yellow";
+
+
+
+
+
+
+
+document.getElementById("body").style.backgroundImage = "url('kabiraa.jpg')";
+
+
